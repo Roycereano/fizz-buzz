9452d02dc1ebb41b8fe9d6e3c98a38e8cdc9b373
Royce Reano <reano.royce@gmail.com>
2021-09-29 15:33:41 -0600

    implemented fizzbuzz values

--- src/main/java/edu/cnm/deepdive/FizzBuzz.java
+++ src/main/java/edu/cnm/deepdive/FizzBuzz.java
@@ -2,16 +2,20 @@ package edu.cnm.deepdive;
 
 public class FizzBuzz {
 
+  public static final String BUZZ_VALUE = "Buzz";
+  public static final String FIZZ_BUZZ_VALUE = "FizzBuzz";
+  public static final String FIZZ_VALUE = "Fizz";
+
   public String getFizzBuzz (int value) {
     String str;
     if (value % 3 == 0) {
       if (value % 5 == 0) {
-        str = "FizzBuzz";
+        str = FIZZ_BUZZ_VALUE;
       } else {
-        str = "Fizz";
+        str = FIZZ_VALUE;
       }
     } else if (value % 5 == 0) {
-        str = "Buzz";
+        str = BUZZ_VALUE;
       } else {
         str = String.valueOf(value);
       }
